bb5c64fafecf80317504659dc6e06bf1e16f6b9e
Carlos Barcelos <carlosbarcelos@rede.ulbra.br>
2021-05-12 18:56:13 -0300

    feat - add new components

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,17 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    SafeAreaView,
     StyleSheet,
     Text,
     View,
 } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { EnviromentButton } from '../components/EnviromentButton';
 
 import { Header } from '../components/Header'
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import api from '../services/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnviromentProps {
+    key: string;
+    title: string;
+}
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+        times: number,
+        repeat_every: string,
+    }
+}
+
 export function PlantSelect() {
+    const [enviroment, setEnviroment] = useState<EnviromentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+    const [enviromentSelected, setEnviromentSelected] = useState('all');
+
+    function handleEnviromentSelected(enviroment: string) {
+        setEnviromentSelected(enviroment);
+
+        if (enviroment === 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant =>
+            plant.environments.includes(enviroment)
+        );
+
+        setFilteredPlants(filtered);
+    }
+
+
+    useEffect(() => {
+        async function fetchEnviroment() {
+            const { data } = await api
+                .get('plants_environments?_sort=title&_order=asc');
+            setEnviroment([
+                {
+                    key: "all",
+                    title: "Todos",
+                },
+                ...data
+            ]);
+        }
+        fetchEnviroment();
+    }, []);
+
+    useEffect(() => {
+        async function fetchPlants() {
+            const { data } = await api
+                .get('plants?_sort=name&_order=asc');
+            setPlants(data);
+        }
+        fetchPlants();
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -23,8 +86,32 @@ export function PlantSelect() {
                     você quer colocar sua planta?
                 </Text>
             </View>
+            <View>
+                <FlatList
+                    data={enviroment}
+                    renderItem={({ item }) => (
+                        <EnviromentButton
+                            title={item.title}
+                            active={item.key === enviromentSelected}
+                            onPress={() => handleEnviromentSelected(item.key)}
+                        />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.enviromentList}
+                />
+            </View>
+            <View style={styles.plantsContainer}>
+                <FlatList
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary data={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                />
+            </View>
         </View>
-
 
     )
 }
@@ -50,4 +137,16 @@ const styles = StyleSheet.create({
         lineHeight: 20,
         color: colors.heading,
     },
+    enviromentList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32,
+    },
+    plantsContainer: {
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center',
+    }
 });
